83e0298bf2848afb2b7c68e1e8af1ad73c701689
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-17 22:30:55 +0000

    Push changes

--- app.py
+++ app.py
@@ -206,6 +206,17 @@ def allowed_file(filename):
 # print(probs)
 # print(classes)
 
+# model, class_to_idx = load_checkpoint('checkpoint_ic_d161.pth')
+
+# def get_prediction(image_bytes):
+#     try:
+#         tensor = transform_image(image_bytes=image_bytes)
+#         outputs = model.forward(tensor)
+#     except Exception:
+#         return 0, 'error'
+#     _, y_hat = outputs.max(1)
+#     predicted_idx = str(y_hat.item())
+#     return class_to_idx[predicted_idx]
 
 @app.route("/")
 def index():
